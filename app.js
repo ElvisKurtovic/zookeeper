8c70dae1f072588016c33177cd4097041266c7d7
ElvisKurtovic <elvis.kurtovic23@gmail.com>
2021-02-15 14:46:38 -0700

    zookeeper

---
+++ app.js
@@ -0,0 +1,368 @@
+// let FemaleCage = []
+// let MaleCage = []
+
+class Alpaca {
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+    ) {
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+    }
+}
+
+class Bat {
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+    ) {
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+    }
+}
+
+class Crocodile {
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+    ) {
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+    }
+}
+
+class Doge {
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+    ) {
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+    }
+}
+
+class Elephant {
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+    ) {
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+    }
+}
+
+class Frog {
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+    ) {
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+    }
+}
+
+class Grasshopper {
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+    ) {
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+    }
+}
+
+class Hiphopanonymous {
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+    ) {
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+    }
+}
+
+class Iguana {
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+    ) {
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+    }
+}
+
+class Jaguar {
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+    ) {
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+    }
+}
+
+class Kangaroo {
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+    ) {
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+    }
+}
+
+class Lobster {
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+    ) {
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+    }
+}
+
+class MountainLlama {
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+    ) {
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+    }
+}
+
+class Narwhal {
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+    ) {
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+    }
+}
+
+class Ocelot {
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+    ) {
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+    }
+}
+
+class Panther {
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+    ) {
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+    }
+}
+
+class Quail {
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+    ) {
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+    }
+}
+
+class Rat {
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+    ) {
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+    }
+}
+
+class Snake {
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+    ) {
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+    }
+}
+
+class Tarantula {
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+    ) {
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+    }
+}
+
+class Urchin {
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+    ) {
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+    }
+}
+
+class Vulture {
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+    ) {
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+    }
+}
+
+class Wombat {
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+    ) {
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+    }
+}
+
+class Xeme {
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+    ) {
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+    }
+}
+
+class Yak {
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+    ) {
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+    }
+}
+
+class Zebra {
+    constructor(
+        name = '',
+        color = '',
+        gender = '',
+    ) {
+        this.name = name;
+        this.color = color;
+        this.gender = gender;
+    }
+}
+
+class MaleCage {
+    constructor() {
+        this.animals = [];
+    }
+}
+
+class FemaleCage {
+    constructor() {
+
+        this.animals = [];
+    }
+
+}
+// console.log(FemaleCage.[])
+
+let Females = new FemaleCage();
+let Males = new MaleCage();
+
+let Albert = new Alpaca("Albert", "Brown", "Male");
+Males.animals.push(Albert)
+console.log(Males)
+let Beatrice = new Bat("Beatrice", "Black", "Female");
+Females.animals.push(Beatrice)
+console.log(Females)
+let Charlie = new Crocodile("Charlie", "Green", "Male");
+Males.animals.push(Charlie)
+let Oscar = new Doge("Doge", "White", "Male");
+Males.animals.push(Oscar)
+let Ellie = new Elephant("Ellie", "Grey", "Female");
+let Franklin = new Frog("Franklin", "Light Green", "Male");
+let Glizzy = new Grasshopper("Glizzy", "Pale Green", "Male");
+let Henry = new Hiphopanonymous("Henry", "Light Grey", "Male");
+let Iggy = new Iguana("Iggy", "Dark Green", "Female");
+let James = new Jaguar("James", "Spotted", "Male");
+let Kenneth = new Kangaroo("Kenneth", "A little Brown", "Male");
+let Lance = new Lobster("Lance", "Red", "Male");
+let Merry = new MountainLlama("Merry", "Black", "Female");
+let Nancy = new Narwhal("Nancy", "Grey", "Female");
+let Ozzy = new Ocelot("Ozzy", "Striped", "Male");
+let SirPurr = new Panther("SirPurr", "Black", "Male");
+let Quandry = new Quail("Quandry", "Brown", "Male");
+let Ratatouille = new Rat("Ratatouille", "Grey", "Male");
+let Slithery = new Snake("Slithery", "Black", "Male");
+let Terrance = new Tarantula("Terrance", "Brown", "Male");
+let Pokey = new Urchin("Pokey", "Black", "Female");
+let Vernon = new Vulture("Vernon", "Black", "Male");
+let Willard = new Wombat("Willard", "Brown", "Male");
+let Xander = new Xeme("Xander", "Black and White", "Male");
+let Yakety = new Yak("Yakety", "Black", "Female");
+let Zena = new Zebra("Zena", "Black and White Stripes", "Female");
+
+
+// FemaleCage.animals.push(Beatrice)
